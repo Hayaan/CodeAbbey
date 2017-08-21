ff9a2d9678b6b493dc7daf3f78100c5eefd1c63a
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-08-21 12:08:01 +0200

    #16

--- 16_avgOfArray.py
+++ 16_avgOfArray.py
@@ -1,8 +1,12 @@
 # http://www.codeabbey.com/index/task_view/average-of-array
 
-
-n = int(input()); resultList = []
+n = int(input()); resultList = []; resString = ""
 for i in range(n):
-    currentString = input()
-    map(int, currentString.split())
-print(max(currentString))
+    currSum = 0
+    currList = list(input().split())
+    for num in currList:
+        currSum += int(num)
+    resultList.append(currSum/(len(currList)-1))
+for resNum in resultList:
+    resString += str(round(resNum)) + " "
+print(resString)
